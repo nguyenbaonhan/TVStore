e7f20d1b0575abd5a3129b82462c1ae030c6a6d2
nhan-nb <nhan-nb@Nhan-NB>
2017-06-09 12:01:14 +0900

    Init

---
+++ TVStore/src/main/java/com/tvstore/TvStoreApplication.java
@@ -0,0 +1,12 @@
+package com.tvstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvStoreApplication.class, args);
+	}
+}
---
+++ TVStore/src/test/java/com/tvstore/TvStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tvstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TvStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
